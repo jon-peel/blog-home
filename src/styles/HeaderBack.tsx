1f68c130ead1015230565161dd5c145d4b8ecd91
Jonathan Peel <me@jonathanpeel.co.za>
2020-04-14 21:37:36 +0300

    header image

--- src/styles/HeaderBack.tsx
+++ src/styles/HeaderBack.tsx
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 import React from 'react';
 
 import { graphql, useStaticQuery } from 'gatsby';
-import BackgroundImage from 'gatsby-background-image'
+import BackgroundImage from 'gatsby-background-image';
 
-// type Props = { images: Array<GatsbyTypes.BackgroundImageQuery['desktop']['childImageSharp']['a']> };
-
-
-/* ${(p: Props) => p.images.map(i => `
-		@media (max-width: ${i.presentationWidth}px) { background-image: url('${i.base64}') }
-	`)} */
+const Image = styled(BackgroundImage)`
+	background-size: cover;
+	background-position-y: center;
+	position: absolute;
+	width: 100%;
+	height: 400px;
+`;
 
 const query = graphql`
 query BackgroundImage {
@@ -33,13 +34,6 @@ query BackgroundImage {
 `;
 
 
-const Image = styled(BackgroundImage)`
-	background-size: cover;
-	background-position-y: center;
-	position: absolute;
-	width: 100%;
-	height: 400px;
-`;
 
 const BackgroundSection = () => {
 	const data = useStaticQuery<GatsbyTypes.BackgroundImageQuery>(query)!;
@@ -54,10 +48,6 @@ const BackgroundSection = () => {
 		<Image fluid={sources as any} className="innerImage" style={{position: 'absolute'}} />
 	);
 };
-
-// import back from '../images/back.jpg';
-
-
 
 const HeaderBack = styled.div.attrs({ children: <BackgroundSection /> })`
 	grid-area: head;
